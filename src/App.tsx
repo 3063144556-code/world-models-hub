6b1868eafbb3e0a4b88d5fa1a877f8849f5a8a81
3063144556-code <3063144556@qq.com>
2026-04-18 10:39:42 +0800

    Fix: consistent named exports, remove duplicate App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Navbar } from '@/components/Navbar';
 import { HeroSection } from '@/components/HeroSection';
-import CategorySection from '@/components/CategorySection';
+import { CategorySection } from '@/components/CategorySection';
 import { TimelineHistory } from '@/components/TimelineHistory';
 import { ProspectScoreSection } from '@/components/ProspectScoreSection';
-import ArchitectureTrendSection from '@/components/ArchitectureTrendSection';
-import TimelineSection from '@/components/TimelineSection';
-import AnonymousComments from '@/components/AnonymousComments';
-import AboutSection from '@/components/AboutSection';
-import Footer from '@/components/Footer';
+import { ArchitectureTrendSection } from '@/components/ArchitectureTrendSection';
+import { TimelineSection } from '@/components/TimelineSection';
+import { AnonymousComments } from '@/components/AnonymousComments';
+import { AboutSection } from '@/components/AboutSection';
+import { Footer } from '@/components/Footer';
 import './App.css';
 
 function App() {
